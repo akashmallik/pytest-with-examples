275115d2baa88b5f8dc89e5d5ead693f65c6b29b
akashmallik <toakashmallik@gmail.com>
2022-03-11 01:30:31 +0600

    PWE-2: Refactor datetime mock

--- tests/test_datetime_now.py
+++ tests/test_datetime_now.py
@@ -2,26 +2,29 @@ import datetime
 
 import pytest
 
-from src.datetime_now import get_now, get_now_with_timezone
-
-FAKE_NOW = datetime.datetime(2025, 1, 1)
+FAKE_NOW = datetime.datetime(2025, 1, 1, 12, 0, 0)
 
 
-@pytest.fixture
-def patch_datetime(monkeypatch):
-    class MockDatetime:
-        @classmethod
-        def now(cls, tz=None):
-            return FAKE_NOW
+@pytest.fixture()
+def patch_datetime(mocker, monkeypatch):
+    """
+    Patch datetime
+    """
+    mocked_datetime = mocker.MagicMock(wrapt=datetime.datetime)
+    mocked_datetime.now.return_value = FAKE_NOW
 
-    monkeypatch.setattr(datetime, "datetime", MockDatetime)
-
-
-class TestGetNow:
-    def test_get_now_success(self, patch_datetime):
-        assert get_now() == "2025-01-01T00:00:00"
+    monkeypatch.setattr(datetime, "datetime", mocked_datetime)
 
 
-class TestGetNowWithTimezone:
-    def test_get_now_with_timezone_success(self, patch_datetime):
-        assert get_now_with_timezone(timezone=datetime.timezone.utc) == "2025-01-01T00:00:00"
+def test_datetime_now(patch_datetime):
+    """
+    Test datetime now without timezone
+    """
+    assert datetime.datetime.now() == FAKE_NOW
+
+
+def test_datetime_now_with_timezone(patch_datetime):
+    """
+    Test datetime now with timezone
+    """
+    assert datetime.datetime.now(tz=datetime.timezone.utc) == FAKE_NOW
